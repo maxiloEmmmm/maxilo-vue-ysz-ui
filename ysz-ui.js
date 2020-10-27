f071cc3accc92bd73be3fe03442ab7bc08fbeec7
maxilozoz <2802146520@qq.com>
2020-10-27 12:08:41 +0800

    rm package

--- ysz-ui.js
+++ ysz-ui.js
@@ -1,12 +1,10 @@
 import components from './src/components'
-import infiniteScroll from 'vue-infinite-scroll'
 import './src/sass/index.scss'
 
 import twComponents from './src/components/tw'
 import './src/sass/tw.scss'
 
 const install = function(vue){
-    vue.use(infiniteScroll)
     
     components.map(component => {
         vue.component(component.name, component)
